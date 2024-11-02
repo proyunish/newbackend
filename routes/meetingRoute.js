25ea46bf2530328740d3462512ae36536440decb
Your Name <yunish@gmail.com>
2024-11-02 18:26:59 +0545

    some changes

--- routes/meetingRoute.js
+++ routes/meetingRoute.js
@@ -4,7 +4,7 @@ const { upload } = require('../middleware/multerConfig');
 const { errorHandler } = require('../middleware/errorHandler');
 const { createMeetings, getallMeetings } = require('../controller/meetingController');
 const router = express.Router()
-router.route("/create").post(isAuthenticated,errorHandler(createMeetings))
+router.route("/create").post(errorHandler(createMeetings))
 router.route("/get").get(isAuthenticated,errorHandler(getallMeetings))
 
 
